052c1626a9c53191e63dfad8ed1715a85ebab8f5
Cruizicredo <viniciusluz07@hotmail.com>
2024-04-23 22:02:36 -0300

    Arquivos certos

---
+++ lib/bottom_nav_with_animated_icons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const Color bottoNavBgColor = Color.fromARGB(255, 206, 64, 130);
+
+class BottomNavWithAnimatedIcons extends StatefulWidget {
+  const BottomNavWithAnimatedIcons({super.key});
+
+  @override
+  State<BottomNavWithAnimatedIcons> createState() =>
+      _BottomNavWithAnimatedIconsState();
+}
+
+class _BottomNavWithAnimatedIconsState
+    extends State<BottomNavWithAnimatedIcons> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
